137c3bc3309d8a560d6e48fb7273f9ad21b27fc0
MaksJv <142892185+MaksJv@users.noreply.github.com>
2025-05-05 19:34:33 +0000

    files book.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 document.addEventListener("partialsLoaded", () => {
   console.log("index.js підключено!");
-  import("./header.js");
-  import("./who-i-am.js");
-  import('./carousel-animated.js');
-  import('./header-intro.js');
-  import('./main.section.hello-there.partial.js');
-  import('./our-slider.js');
-  import("./logo-modal.js");
-  import("./main.section.collaboration.button.js");
+  // import("./header.js");
+  // import("./who-i-am.js");
+  // import('./carousel-animated.js');
+  // import('./header-intro.js');
+  // import('./main.section.hello-there.partial.js');
+  // import('./our-slider.js');
+  // import("./logo-modal.js");
+  // import("./main.section.collaboration.button.js");
   import("./form-submit.js");
-  import("./admin.js");
 });
